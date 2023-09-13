d76942fa5a1189318d8eeafe6563f51a8c4a8aa8
MateoBacci <mateobacci06@gmail.com>
2023-09-13 11:43:35 -0300

    tp1 ej2

--- TP1/src/AST.hs
+++ TP1/src/AST.hs
@@ -6,25 +6,26 @@ type Variable = String
 -- Expresiones, aritmeticas y booleanas
 data Exp a where
   -- Expresiones enteras
-  Const ::Int -> Exp Int
-  Var ::Variable -> Exp Int
-  UMinus ::Exp Int -> Exp Int
-  Plus ::Exp Int -> Exp Int -> Exp Int
-  Minus ::Exp Int -> Exp Int -> Exp Int
-  Times ::Exp Int -> Exp Int -> Exp Int
-  Div ::Exp Int -> Exp Int -> Exp Int
-  
+  Const :: Int -> Exp Int
+  Var :: Variable -> Exp Int
+  UMinus :: Exp Int -> Exp Int
+  Plus :: Exp Int -> Exp Int -> Exp Int
+  Minus :: Exp Int -> Exp Int -> Exp Int
+  Times :: Exp Int -> Exp Int -> Exp Int
+  Div :: Exp Int -> Exp Int -> Exp Int
+  ESeq :: Exp Int -> Exp Int -> Exp Int
+  EAssgn :: Variable -> Exp Int -> Exp Int
 
   -- Expreseiones booleanas
-  BTrue ::Exp Bool
-  BFalse ::Exp Bool
-  Lt ::Exp Int -> Exp Int -> Exp Bool
-  Gt ::Exp Int -> Exp Int -> Exp Bool
-  And ::Exp Bool -> Exp Bool -> Exp Bool
-  Or ::Exp Bool -> Exp Bool -> Exp Bool
-  Not ::Exp Bool -> Exp Bool
-  Eq ::Exp Int -> Exp Int -> Exp Bool
-  NEq ::Exp Int -> Exp Int -> Exp Bool
+  BTrue :: Exp Bool
+  BFalse :: Exp Bool
+  Lt :: Exp Int -> Exp Int -> Exp Bool
+  Gt :: Exp Int -> Exp Int -> Exp Bool
+  And :: Exp Bool -> Exp Bool -> Exp Bool
+  Or :: Exp Bool -> Exp Bool -> Exp Bool
+  Not :: Exp Bool -> Exp Bool
+  Eq :: Exp Int -> Exp Int -> Exp Bool
+  NEq :: Exp Int -> Exp Int -> Exp Bool
 
 deriving instance Show (Exp a)
 deriving instance Eq (Exp a)
@@ -39,7 +40,7 @@ data Comm
   | Repeat Comm (Exp Bool)
   deriving (Show, Eq)
 
-pattern IfThen :: Exp Bool -> Comm -> Comm
+pattern IfThen ::  Exp Bool -> Comm -> Comm
 pattern IfThen b c = IfThenElse b c Skip
 
 data Error = DivByZero | UndefVar deriving (Eq, Show)
